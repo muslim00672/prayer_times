e44901c15e7dd25c5e68ae62cbc80924e2a80fef
muslim00672 <farag00672@gmail.com>
2025-04-18 18:07:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,315 @@
+const L0 = [
+    [175347046, 0, 0],
+    [3341656, 4.6692568, 6283.07585],
+    [34894, 4.6261, 12566.1517],
+    [3497, 2.7441, 5753.3849],
+    [3418, 2.8289, 3.5231],
+    [3136, 3.6277, 77713.7715],
+    [2676, 4.4181, 7860.4194],
+    [2343, 6.1352, 3930.2097],
+    [1324, 0.7425, 11506.7698],
+    [1273, 2.0371, 529.691],
+    [1199, 1.1096, 1577.3435],
+    [990, 5.233, 5884.927],
+    [902, 2.045, 26.298],
+    [857, 3.508, 398.149],
+    [780, 1.179, 5223.694],
+    [753, 2.533, 5507.553],
+    [505, 4.583, 18849.228],
+    [492, 4.205, 775.523],
+    [357, 2.92, 0.067],
+    [317, 5.849, 11790.629],
+    [284, 1.899, 796.298],
+    [271, 0.315, 10977.079],
+    [243, 0.345, 5486.778],
+    [206, 4.806, 2544.314],
+    [205, 1.869, 5573.143],
+    [202, 2.458, 6069.777],
+    [156, 0.833, 213.299],
+    [132, 3.411, 2942.463],
+    [126, 1.083, 20.775],
+    [115, 0.645, 0.98],
+    [103, 0.636, 4694.003],
+    [102, 0.976, 15720.839],
+    [102, 4.267, 7.114],
+    [99, 6.21, 2146.17],
+    [98, 0.68, 155.42],
+    [86, 5.98, 161000.69],
+    [85, 1.3, 6275.96],
+    [85, 3.67, 71430.7],
+    [80, 1.81, 17260.15],
+    [79, 3.04, 12036.46],
+    [75, 1.76, 5088.63],
+    [74, 3.5, 3154.69],
+    [74, 4.68, 801.82],
+    [70, 0.83, 9437.76],
+    [62, 3.98, 8827.39],
+    [61, 1.82, 7084.9],
+    [57, 2.78, 6286.6],
+    [56, 4.39, 14143.5],
+    [56, 3.47, 6279.55],
+    [52, 0.19, 12139.55],
+    [52, 1.33, 1748.02],
+    [51, 0.28, 5856.48],
+    [49, 0.49, 1194.45],
+    [41, 5.37, 8429.24],
+    [41, 2.4, 19651.05],
+    [39, 6.17, 10447.39],
+    [37, 6.04, 10213.29],
+    [37, 2.57, 1059.38],
+    [36, 1.71, 2352.87],
+    [36, 1.78, 6812.77],
+    [33, 0.59, 17789.85],
+    [30, 0.44, 83996.85],
+    [30, 2.74, 1349.87],
+    [25, 3.16, 4690.48],
+];
+const L1 = [
+    [628331966747, 0, 0],
+    [206059, 2.678235, 6283.07585],
+    [4303, 2.6351, 12566.1517],
+    [425, 1.59, 3.523],
+    [119, 5.796, 26.298],
+    [109, 2.966, 1577.344],
+    [93, 2.59, 18849.23],
+    [72, 1.14, 529.69],
+    [68, 1.87, 398.15],
+    [67, 4.41, 5507.55],
+    [59, 2.89, 5223.69],
+    [56, 2.17, 155.42],
+    [45, 0.4, 796.3],
+    [36, 0.47, 775.52],
+    [29, 2.65, 7.11],
+    [21, 5.34, 0.98],
+    [19, 1.85, 5486.78],
+    [19, 4.97, 213.3],
+    [17, 2.99, 6275.96],
+    [16, 0.03, 2544.31],
+    [16, 1.43, 2146.17],
+    [15, 1.21, 10977.08],
+    [12, 2.83, 1748.02],
+    [12, 3.26, 5088.63],
+    [12, 5.27, 1194.45],
+    [12, 2.08, 4694],
+    [11, 0.77, 553.57],
+    [10, 1.3, 6286.6],
+    [10, 4.24, 1349.87],
+    [9, 2.7, 242.73],
+    [9, 5.64, 951.72],
+    [8, 5.3, 2352.87],
+    [6, 2.65, 9437.76],
+    [6, 4.67, 4690.48],
+];
+const L2 = [
+    [52919, 0, 0],
+    [8720, 1.0721, 6283.0758],
+    [309, 0.867, 12566.152],
+    [27, 0.05, 3.52],
+    [16, 5.19, 26.3],
+    [16, 3.68, 155.42],
+    [10, 0.76, 18849.23],
+    [9, 2.06, 77713.77],
+    [7, 0.83, 775.52],
+    [5, 4.66, 1577.34],
+    [4, 1.03, 7.11],
+    [4, 3.44, 5573.14],
+    [3, 5.14, 796.3],
+    [3, 6.05, 5507.55],
+    [3, 1.19, 242.73],
+    [3, 6.12, 529.69],
+    [3, 0.31, 398.15],
+    [3, 2.28, 553.57],
+    [2, 4.38, 5223.69],
+    [2, 3.75, 0.98],
+];
+const L3 = [
+    [289, 5.844, 6283.076],
+    [35, 0, 0],
+    [17, 5.49, 12566.15],
+    [3, 5.2, 155.42],
+    [1, 4.72, 3.52],
+    [1, 5.3, 18849.23],
+    [1, 5.97, 242.73],
+];
+const L4 = [
+    [114, 3.142, 0],
+    [8, 4.13, 6283.08],
+    [1, 3.84, 12566.15],
+];
+const L5 = [
+    [1, 3.14, 0],
+];
+
+function mod(x){
+    return (x % 1 + 1) % 1;
+}
+
+function radians(angle){
+    return angle * Math.PI / 180;
+}
+
+function degrees(x){
+    return x * 180 / Math.PI;
+}
+
+function sin_r(angle) {
+    return Math.sin(radians(angle));
+}
+
+function cos_r(angle) {
+    return Math.cos(radians(angle));
+}
+
+function tan_r(angle) {
+    return Math.tan(radians(angle));
+}
+
+function asin_d(x) {
+    return degrees(Math.asin(x));
+}
+
+function acos_d(x) {
+    return degrees(Math.acos(x));
+}
+
+function atan_d(x) {
+    return degrees(Math.atan(x));
+}
+
+function atan2_d(y, x) {
+    return degrees(Math.atan2(y, x));
+}
+
+function poly(base, coeffs) {
+    return coeffs.reduce((sum, coeff, i) => sum + coeff * base**i, 0);
+}
+
+function cnvrt_t(t, stamp = new Date()) {
+    t %= 1;
+    let hour = t * 24;
+    let minute = hour % 1 * 60;
+    let second = minute % 1 * 60;
+    [hour, minute] = [hour, minute].map(t => parseInt(t));
+    second = Math.round(second);
+    stamp.setHours(hour, minute, stamp.getSeconds(), 0);
+    stamp = new Date(stamp.getTime() - stamp.getTimezoneOffset() * 60000);
+    if (second === 60) {
+        stamp.setSeconds(0);
+        stamp = new Date(stamp.getTime() + 60000);
+    } else {
+        stamp.setSeconds(second);
+    }
+    return stamp;
+}
+
+function sundial(lat, long, stamp = new Date()) {
+    stamp = new Date(stamp.getTime() + stamp.getTimezoneOffset() * 60000);
+    let year = stamp.getFullYear();
+    let month = stamp.getMonth() + 1;
+    let day = stamp.getDate();
+    const hour = stamp.getHours();
+    const minute = stamp.getMinutes();
+    const second = stamp.getSeconds();
+    const millisecond = stamp.getMilliseconds();
+    if (month <= 2) {
+        year--;
+        month += 12;
+    }
+    const day_frct = hour / 24 + minute / 1440 + second / 86400 + millisecond / 86400000;
+    day += day_frct;
+    const A = Math.floor(year / 100);
+    const B = 2 - A + Math.floor(A / 4);
+    const JD = parseInt(365.25 * (year + 4716)) + parseInt(30.6001 * (month + 1)) + day + B - 1524.5;
+
+    const JC = (JD - 2451545) / 36525;
+    const JM = JC / 10;
+    const U = JM / 10;
+
+    const ε0 = 84381.448 - 4680.93 * U - 1.55 * U**2 + 1999.25 * U**3 - 51.38 * U**4 - 249.67 * U**5 - 39.05 * U**6 + 7.12 * U**7 + 27.87 * U**8 + 5.79 * U**9 + 2.45 * U**10;
+    const ε = ε0 / 3600;
+    const z = 360.98564736629 * 36525;
+    const ν = 280.46061837 + z * JC + 0.000387933 * JC**2 - JC**3 / 38710000;
+
+    let L = [L0, L1, L2, L3, L4, L5];
+    const lst = [];
+    for (const arr of L) {
+        let sm = 0;
+        for (const row of arr) {
+            sm += row[0] * Math.cos(row[1] + row[2] * JM);
+        }
+        lst.push(sm);
+    }
+    L = poly(JM, lst) / 100000000;
+    L = degrees(L);
+
+    const λ = L + 180;
+    const α = atan2_d(sin_r(λ) * cos_r(ε), cos_r(λ));
+    const δ = asin_d(sin_r(ε) * sin_r(λ));
+    const H = ν - α + long;
+    const e = asin_d(sin_r(lat) * sin_r(δ) + cos_r(lat) * cos_r(δ) * cos_r(H));
+    return [e, ν, H, α, δ];
+}
+
+function timeof(angle, lat, long, stamp = new Date(), elev = 0) {
+    stamp.setHours(0, 0, 0, 0);
+    stamp = new Date(stamp.getTime() - stamp.getTimezoneOffset() * 60000);
+    let [ν, m0, α0, δ0] = sundial(lat, long, stamp).slice(1);
+    const [α1, δ1] = sundial(lat, long, new Date(stamp.getTime() + 86400000)).slice(3);
+    const [α_1, δ_1] = sundial(lat, long, new Date(stamp.getTime() - 86400000)).slice(3);
+    if (angle === 'h0') {
+        const R = 6378140;
+        let d = (Math.abs(elev) * (Math.abs(elev) + 2 * R))**0.5;
+        if (elev < 0) {
+            d *= -1;
+        }
+        angle = -0.8333 - asin_d(d / R);
+    }
+    const H0 = acos_d((sin_r(angle) - sin_r(lat) * sin_r(δ0)) / (cos_r(lat) * cos_r(δ0)));
+    m0 *= -1;
+    const m1 = m0 - H0;
+    const m2 = m0 + H0;
+
+    function intr(x) {
+        if (Math.abs(x) > 2) {
+            return mod(x);
+        }
+        return x;
+    }
+
+    const a = intr(α0 - α_1);
+    const b = intr(α1 - α0);
+    const a_ = intr(δ0 - δ_1);
+    const b_ = intr(δ1 - δ0);
+    const δ = [];
+    const H = [];
+    const h = [];
+    let i = 0;
+    for (let m of [m0, m1, m2]) {
+        m = mod(m / 360);
+        const ν_i = ν + 360.985647 * m;
+        const α = α0 + m / 2 * (a + b + m * (b - a));
+        δ.push(δ0 + m / 2 * (a_ + b_ + m * (b_ - a_)));
+        H.push(ν_i - α + long);
+        h.push(asin_d(sin_r(lat) * sin_r(δ[i]) + cos_r(lat) * cos_r(δ[i]) * cos_r(H[i])));
+        i++;
+    }
+    const T = m0 - H[0];
+    const R = m1 + (h[1] - angle) / (cos_r(δ[1]) * cos_r(lat) * sin_r(H[1]));
+    const S = m2 + (h[2] - angle) / (cos_r(δ[2]) * cos_r(lat) * sin_r(H[2]));
+    return [R, S, T].map(m => mod(m / 360));
+}
+
+function prayer_times(lat, long, stamp = new Date(), elev) {
+    const [shurooq, maghrib, dhuhr] = timeof('h0', lat, long, stamp, elev);
+    const [fajr18, isha18] = timeof(-18, lat, long, stamp).slice(0, 2);
+    const [fajr15, isha15] = timeof(-15, lat, long, stamp).slice(0, 2);
+    const dhuhr_angle = sundial(lat, long, cnvrt_t(dhuhr, stamp))[0];
+    const x = 1;
+    const asr_angle = atan2_d(tan_r(dhuhr_angle), 1 + x * tan_r(dhuhr_angle));
+    const asr = timeof(asr_angle, lat, long, stamp)[1];
+    const muntasaf = timeof('h0', lat, long, new Date(stamp.getTime() + 86400000))[2] - 0.5;
+    return [fajr18, fajr15, shurooq, dhuhr, asr, maghrib, isha15, isha18, muntasaf]
+    .map(time => cnvrt_t(time, new Date(stamp.getTime()))
+    .toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true }).replace('AM', 'ص').replace('PM', 'م')
+    );
+}
